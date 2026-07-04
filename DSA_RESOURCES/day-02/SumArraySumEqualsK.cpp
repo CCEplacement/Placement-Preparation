ff14699acfd98efeda7c67f51abb1f58b4f042bb
Gayu5-12 <gayathridevi.2312@gmail.com>
2026-07-04 10:18:13 +0530

    day-02 session notes/prefixSum

---
+++ DSA_RESOURCES/day-02/SumArraySumEqualsK.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0 ;
+        int sum=0;
+       
+        unordered_map<int, int>mp;
+         mp[0]=1;
+        for(int x: nums){
+            sum+=x;
+            if(mp.find(sum-k) != mp.end()){
+                count+=mp[sum - k];
+            } 
+            mp[sum]++;
+        }
+        return count;
+        
+    }
+};

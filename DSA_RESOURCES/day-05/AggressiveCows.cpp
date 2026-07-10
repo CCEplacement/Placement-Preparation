dd1e1901bc9683979e96e32d4a0f24a90fb036aa
Deepiga0706 <deepiga.rs2024cce@sece.ac.in>
2026-07-10 14:54:36 +0530

    day-05 DSA session: Binary Search

---
+++ DSA_RESOURCES/day-05/AggressiveCows.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    bool canPlace(vector<int>& arr, int k, int dist)
+    {
+        int cows = 1;
+        int lastPlaced = arr[0];
+
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]-lastPlaced>=dist)
+            {
+                cows++;
+                lastPlaced=arr[i];
+            }
+
+            if(cows==k)
+                return true;
+        }
+
+        return false;
+    }
+
+    int aggressiveCows(vector<int> &arr, int k)
+    {
+        sort(arr.begin(),arr.end());
+
+        int low = 1;
+        int high = arr.back()-arr.front();
+
+        int ans = 0;
+
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+
+            if(canPlace(arr,k,mid))
+            {
+                ans = mid;
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
